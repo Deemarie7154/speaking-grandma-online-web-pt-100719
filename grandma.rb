df6743ff645eebc0a18b79ed098ecb9c4d11cfae
Dwight Marie <dwightmarie@gmail.com>
2019-10-10 02:13:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,13 @@
 
 def speak_to_grandma(phrase)
   
-  if phrase == phrase.upcase
+  if phrase === "I LOVE YOU GRANDMA!"
+  
+  return "I LOVE YOU TOO PUMPKIN!"
+    
+  elsif
+    
+    phrase == phrase.upcase
     
     return "NO, NOT SINCE 1938!"
     
@@ -25,9 +31,7 @@ def speak_to_grandma(phrase)
   return "HUH?! SPEAK UP, SONNY!"
   
 else 
-  phrase === "I LOVE YOU GRANDMA!"
   
-  return "I LOVE YOU TOO PUMPKIN!"
   
    end
  end
